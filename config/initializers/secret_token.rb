4f8485f777ea860948dee261ba0f232ab87796f6
Miguel Martinez <m.martinez2790@gmail.com>
2012-01-18 08:27:38 -0500

    l

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProyectoAIS::Application.config.secret_token = 'e9f27b965ecf15d978c4da6d6cbbf4cdab4ff7365a15b9000a2224a76a1afc6997f528d466266886d7ee3e798c5f996d8b82a4cfa123b549f36d29c2e3ca0075'
